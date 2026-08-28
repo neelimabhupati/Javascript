7016bd2b02816f8f7c35cbe0c8aa357083213645
neelimabhupati <neelimadocument@gmail.com>
2026-08-28 14:57:29 +0200

    Neelima_DOM

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -1,3 +1,41 @@
-let title =document.getElementById('website_title');
+// let title =document.getElementById('website_title');
+// title.innerHTML = "Neu Title";
 
-console.log(title);
+// document.getElementById('test_div').innerHTML= '<p>Neelima</p>';
+
+// const container = document.getElementById('button_container');
+
+// container.innerHTML = '<button id= "myButton" onClick= "handleClick()"> Neelimabutton </button>'
+
+// function handleClick(){
+
+//     console.log('Button clicked via inline event');
+//     myButton.disabled = true;
+
+// }
+
+
+//document.createElement()
+
+const container = document.getElementById('button_container');
+
+
+const myButton = document.createElement('button');
+
+
+myButton.id = 'myButton';
+myButton.style.backgroundColor = 'red';
+myButton.textContent = 'Click me';
+
+// 4. బటన్‌కు Click Event జత చేయడం
+myButton.addEventListener('click', function () {
+    console.log('Button clicked using createElement!');
+    myButton.disabled = true; // క్లిక్ చేయగానే బటన్ డిజేబుల్ అవుతుంది
+});
+
+// 5. తయారు చేసిన బటన్‌ను container DIV లోపలికి చేర్చడం
+container.appendChild(myButton);
+
+{/* <div id="button_container">
+    <button id="myButton">Neelimabutton</button>  <!-- బటన్ ఇక్కడికి వచ్చి చేరుతుంది -->
+</div> */}
