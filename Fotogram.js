26d5a642766391b7cbec46881386e3c749f26bf9
neelimabhupati <neelimadocument@gmail.com>
2026-09-06 17:30:01 +0200

    Pushing Fotogram files

---
+++ Fotogram.js
@@ -0,0 +1,60 @@
+let currentPhotoIndex = 0;
+
+function renderPhotos() {
+    // HTML lo unna #content div element ni select chestam
+    let container = document.getElementById('content');
+
+    //photo container ni clear chestam
+    container.innerHTML = '';
+
+    // loop for photoList in db.js
+
+    for(let i=0; i< photoList.length ; i++){
+
+        container.innerHTML += `<div id= "photo${i}" class= "photo-card" onClick= "openPhoto(${i})" style= "background-image: url('${photoList[i]}');"> </div>`;
+    }
+}
+
+// To open photo function
+
+function openPhoto(index){
+    currentPhotoIndex = index;
+
+    updatedPopup(); // calling helper function.
+    document.getElementById('popup').classList.remove('d-none');
+}
+
+//To update popup details function
+function updatedPopup(){
+    let imgPath = photoList[currentPhotoIndex];
+    let fileName = imgPath.split('/').pop();
+
+    document.getElementById('popup_img').src= imgPath;
+    document.getElementById('photo_title').innerHTML= fileName;
+    document.getElementById('photo-counter').innerText = `${currentPhotoIndex + 1 }/ ${photoList.length}`;
+}
+
+//next photo function
+function nextPhoto(){
+    currentPhotoIndex ++;
+    if(currentPhotoIndex >= photoList.length){ // if 11 >= 11 then photo index 0 will come
+        currentPhotoIndex = 0; 
+    } 
+    updatedPopup();
+}
+
+//previous photo function
+function prevPhoto(){
+    currentPhotoIndex --;
+    if(currentPhotoIndex < 0) {
+        currentPhotoIndex = photoList.length -1; // if -1 < 0, index value will become 11.
+    }
+    updatedPopup();
+}
+
+// close the popup function
+function closePhoto(){
+    document.getElementById('popup').classList.add('d-none');
+}
+
+
---
+++ db.js
@@ -0,0 +1,14 @@
+let photoList = [
+    'assets/Images/Kids_photos/Baby1.jpg',
+    'assets/Images/Kids_photos/Baby2.jpg',
+    'assets/Images/Kids_photos/Baby3.jpg',
+    'assets/Images/Kids_photos/Baby4.jpg',
+    'assets/Images/Kids_photos/Baby5.jpg',
+    'assets/Images/Kids_photos/Baby6.jpg',
+    'assets/Images/Kids_photos/Baby7.jpg',
+    'assets/Images/Kids_photos/Baby8.jpg',
+    'assets/Images/Kids_photos/Baby9.jpg',
+    'assets/Images/Kids_photos/Baby10.jpg',
+    'assets/Images/Kids_photos/Baby11.jpg',
+    'assets/Images/Kids_photos/Baby12.jpg'
+];
