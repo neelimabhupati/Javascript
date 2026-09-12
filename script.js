18e57f954872143f11238e40df1b296358f5cc22
neelimabhupati <neelimabhupatide@gmail.com>
2026-09-12 09:09:01 +0200

    04 - Objects: keys und entries updated

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+// let myObject = {
+//     'name': 'Flo',
+//     'age': 46, 
+//     // 'job1':{
+//     //     'name': 'Neelima',
+//     //     'title': 'AI front-end developer'
+//     // },
+
+//     'logJob': function (number) {
+//         console.log('Neelima ' + number);
+//     },
+//     'good_girl': true,
+// };
+
+// // let myVar = 'job1';
+
+// // console.table(myObject);
+
+// myObject.logJob(233);
+// output Neelima 233
+
 let myObject = {
     'name': 'Flo',
-    'age': 46, 
-    'job1':{
-        'name': 'Neelima',
-        'title': 'AI front-end developer'
-    },
+    'age': 45,
+    'job': 'Neelima',
     'good_girl': true,
 };
 
-let myVar = 'job1';
+let objKeys = Object.keys(myObject)
+console.table(objKeys);
 
-console.log(myObject[myVar]);
+let ourarray = []
+
+for (let index = 0; index < objKeys.length; index++) {
+    const element = objKeys[index];
+    ourarray.push(myObject[objKeys[index]])   
+}
+
+console.table(ourarray);
