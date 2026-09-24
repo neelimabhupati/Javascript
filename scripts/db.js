df4ae818301bff2331f4ba29b7256aa25517f76b
neelimabhupati <neelimabhupatide@gmail.com>
2026-09-24 14:07:02 +0200

    db.js file updated the db list

---
+++ scripts/db.js
@@ -0,0 +1,77 @@
+let menuDetails_Array = [
+    {
+        foodCategories: "Starters & Appetizers",
+        items: [
+            {
+                foodItem_image: '/assets/images/chilli chicken.jpg',
+                foodItem_name: "Chilli Chicken",
+                foodItem_price: 18.00,
+                foodItem_description: "Crispy and spicy chicken.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/Fish Fry.jpg',
+                foodItem_name: "Fish Fry",
+                foodItem_price: 17.99,
+                foodItem_description: "Crispy South Indian fish fry.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/freid rice.jpg',
+                foodItem_name: "Prawn Fry",
+                foodItem_price: 19.79,
+                foodItem_description: "Pan-fried spicy prawns.",
+                add_To_cart: false,
+            },
+        ],
+
+        foodCategories: "Biryanis",
+        items: [
+            {
+                foodItem_image: '/assets/images/chicken biryani.jpg',
+                foodItem_name: "chicken biryani",
+                foodItem_price: 16.90,
+                foodItem_description: "Spicy basmati rice with chicken.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/mutton biryani.jpg',
+                foodItem_name: "mutton biryani",
+                foodItem_price: 21.99,
+                foodItem_description: "Traditional slow-cooked mutton rice.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/freid rice.jpg',
+                foodItem_name: "freid rice biryani",
+                foodItem_price: 15.89,
+                foodItem_description: "Tasty veg fried rice.",
+                add_To_cart: false,
+            },
+        ],
+        foodCategories: "Desserts",
+        items: [
+            {
+                foodItem_image: '/assets/images/apricot delight.webp',
+                foodItem_name: "Apricot Delight",
+                foodItem_price: 8.99,
+                foodItem_description: "Rich apricot sweet dessert.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/gulab jamun.jpg',
+                foodItem_name: "Gulab Jamun",
+                foodItem_price: 9.99,
+                foodItem_description: "Soft sweet milk sweet.",
+                add_To_cart: false,
+            },
+            {
+                foodItem_image: '/assets/images/Rasagulla.jpg',
+                foodItem_name: "Rasagulla",
+                foodItem_price: 5.50,
+                foodItem_description: "Tasty veg fried rice.",
+                add_To_cart: false,
+            },
+        ]
+    }
+]
